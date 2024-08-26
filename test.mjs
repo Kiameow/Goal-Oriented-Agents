164940e9e6eabdec89d31679a1969517f6253c48
kiameow <zjzootl@qq.com>
2024-08-26 23:18:47 +0800

    feat: make the procedure more general and viable

--- test.mjs
+++ test.mjs
@@ -1,7 +1,34 @@
 import { globalAgents } from "./core/agents/Agent.mjs";
 import globalTime from "./core/globalTime.mjs";
 
-globalTime.setTime(0, 10, 10);
-await globalAgents.get('Maria_Lopez').getDailyPlans();
-await globalAgents.get('Maria_Lopez').getHourlyPlans();
-await globalAgents.get('Maria_Lopez').getNextAction();
+let steps = 100;
+while (steps--) {
+    if (globalTime.isNewDay()) {
+        await performDailyAndHourlyPlans();
+    }
+    await performNextAction();
+    globalTime.updateTime();
+    globalTime.saveToFile();
+}
+
+
+async function performDailyAndHourlyPlans() {
+    const planPromises = Array.from(globalAgents.values()).map(async (agent) => {
+        await agent.getDailyPlans();
+        await agent.getHourlyPlans();
+        await agent.saveToInstantMemory();
+    });
+
+    // 等待所有 Agents 完成 getDailyPlans 和 getHourlyPlans 调用
+    await Promise.all(planPromises);
+}
+
+async function performNextAction() {
+    const actionPromises = Array.from(globalAgents.values()).map(async (agent) => {
+        await agent.getNextAction();
+        await agent.saveNextAction();
+    });
+
+    // 等待所有 Agents 完成 getNextAction 调用
+    await Promise.all(actionPromises);
+}
